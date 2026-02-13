5656052e2cbf461209346bc42fd12795d932bfca
IOSIPKOIN <146315141+IOSIPKOIN@users.noreply.github.com>
2026-02-13 16:49:47 +0300

    Enhance balance command with error handling
    Added error handling for message parsing and improved user feedback.

--- handlers/balance.py
+++ handlers/balance.py
@@ -9,5 +9,23 @@ router = Router()
 
 @router.message(Command("bal"))
 async def bal(message: Message):
-    path = await render_html("balance.html", "balance.png")
+    try:
+        _, data = message.text.split("/bal", 1)
+        user, balance, failed, success = [x.strip() for x in data.split(",")]
+    except:
+        await message.answer(
+            "Формат:\n/bal Имя, Баланс, Неудачная_сумма, Успешная_операция\n\n"
+            "Пример:\n/bal Alex, 12500, 5400, 2000"
+        )
+        return
+
+    path = await render_html(
+        "balance.html",
+        "balance.png",
+        user=user,
+        balance=balance,
+        failed=failed,
+        success=success
+    )
+
     await message.answer_photo(photo=open(path, "rb"))
